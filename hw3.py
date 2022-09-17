0bb834a7df9faa2cfe24dddd7c579ffeda46f2fe
rgirl0801 <52407432+rgirl0801@users.noreply.github.com>
2022-09-17 18:04:27 -0400

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,25 +1,69 @@
-# 3.1
-my_list = ['a', 'b', [1, 2, 3], 'd']
-[[print(i) for i in x] for x in my_list if isinstance(x, list)]
+''' 3.1. Дан список my_list = ['a', 'b', [1, 2, 3], 'd'].
+ Распечатайте значения 1, 2, 3'''
 
-# 3.2
+my_list = ['a', 'b', [1, 2, 3], 'd']
+# print('\n'.join(map(str, my_list)))
+
+print(*my_list, sep=" ")
+'''3.2 Дан список list_1 = ['Hi', 'ananas', 2, None, 75, 'pizza', 36, 100]
+    - получите сумму всех чисел,
+    - распечатайте все строки, где есть буква 'a'''
+
 list_1 = ['Hi', 'ananas', 2, None, 75, 'pizza', 36, 100]
 print(sum([x for x in list_1 if isinstance(x, int)]))
-[print(x) for x in list_1 if isinstance(x, str) and x.find('a') != -1]
+print([x for x in list_1 if isinstance(x, str) and 'a' in x])
 
-# 3.3
-animals = ['cat', 'dog', 'horse', 'cow']
-print(tuple(animals))
+'''3.3. Превратите лист ['cat', 'dog', 'horse, 'cow'] в кортеж'''
 
-# 3.4
-family_1 = input('Введите членов первой семьи через запятую\n')
-family_1_arr = family_1.split(',')
-family_2 = input('Введите членов второй семьи через запятую\n')
-family_2_arr = family_2.split(',')
-print('Equal') if len(family_1_arr) == len(family_2_arr) else print(family_1_arr) if len(family_1_arr) > len(family_2_arr) else print(family_2_arr)
+list_2 = ['cat', 'dog', 'horse', 'cow']
+list_21 = tuple(list_2)
+print(type(list_21))
 
-# 3.5
-film = {'title': 'House M.D.', 'director': 'David Shore', 'year': '2004–2012', 'budget': 'unknown', 'main_actor': 'Hugh Laurie', 'slogan': 'Genius has side effects'}
-print(film.keys())
-print(film.values())
-print(film.items())
+'''3.4. Напишите программу, которая определяет, какая семья больше. 
+      1) Программа имеет два input() - например, family_1, family_2. 
+      2) Членов семьи нужно перечислить через запятую. 
+     Ожидаемый результат - программа выводит семью с бОльшим составом. Если состав одинаковый, print("Equal') '''
+
+family_1 = input().split(',')
+family_2 = input().split(',')
+if len(family_1) == len(family_2):
+    print("Equal")
+elif len(family_1) > len(family_2):
+    print('family_1')
+else:
+    print('family_2')
+
+'''3.5. Создайте словарь film c ключами title, director, year, budget, main_actor, slogan. 
+В значения можете передать информацию о вашем любимом фильме. 
+    - распечатайте только ключи
+    - распечатайте только значения
+    - распечатайте пары ключ - значение'''
+
+my_film = {'title': 'Inception.', 'actor': 'Leo DiCaprio',
+           'genre': 'thriller', 'director': 'Christopher Nolan'}
+print(my_film.keys())
+print(my_film.values())
+print(my_film.items())
+
+'''3.6. Найдите сумму всех значений в словаре 
+my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}'''
+
+my_dictionary = {'num1': 375, 'num2': 567, 'num3': -37, 'num4': 21}
+print(sum([value for value in my_dictionary.values()]))
+
+'''3.7. Удалите повторяющиеся значения из списка [1, 2, 3, 4, 5, 3, 2, 1] '''
+
+my_list4 = [1, 2, 3, 4, 5, 3, 2, 1]
+print(set(my_list4))
+
+'''3.8. Даны два множества: set1 = {'a', 'z', 1, 5, 9, 12, 100, 'b'},
+                            set2 = {5, 'z', 1, 8, 9, 21, 100, 'l', 785}
+     - найдите значения, которые встречаются в обоих множествах
+     - найдите значения, которые не встречаются в обоих множествах
+     - проверьте являются ли эти множества подмножествами друг друга '''
+
+set1 = {'a', 'z', 1, 5, 9, 12, 100, 'b'}
+set2 = {5, 'z', 1, 8, 9, 21, 100, 'l', 785}
+print(set1 & set2)
+print(set1 ^ set2)
+print(set1 < set2)
